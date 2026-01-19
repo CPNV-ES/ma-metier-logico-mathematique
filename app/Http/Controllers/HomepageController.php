39840839201b2959072e399c4e060742bb980a48
Camille <camille.bachmann@eduvaud.ch>
2026-01-19 10:52:25 +0100

    fix error handler on class_code

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
+
 class HomepageController extends Controller
 {
     //appelle la page d'accueil
@@ -32,9 +33,23 @@ class HomepageController extends Controller
     //redirige vers une classe en fonction du code
     public function resolveClassByCode(Request $request){
         $class_code = $request->class_code;
-        $SchoolClass = \App\Models\SchoolClass::query()
-            ->where('class_code',$class_code)
-            ->first();
+        //Met class_code en requis --> il ne peut pas être null
+        $request->validate([
+            'class_code' => 'required'
+        ], [
+        //Texte de l'erreur qui s'affichera
+            'class_code.required' => 'Veuillez entrer un code de classe.'
+        ]);
+        //Va chercher la classe grâce au code
+        $SchoolClass = \App\Models\SchoolClass::query()->where('class_code',$class_code)->first();
+        //La recherche a donné une erreur --> le code de classe n'existe pas dans la BD
+        if (!$SchoolClass) {
+            return back()->withErrors([
+            //Texte de l'erreur qui s'affichera
+            'class_code' => 'Ce code de classe est invalide.'
+            ]);
+            }
+        //Tout s'est bien passé --> dirigé vers la bonne classe
         return redirect()->route('homepage.classes', ['name'=>$SchoolClass->name]);
     }
 }
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -8,8 +8,14 @@
     <a href="{{route('homepage.newAccount')}}">New Account</a>
     <form action="" method="post">
         @csrf
-        <!-- Champ pour mdp de classe, type number pour que les enfants voient ce qu'ils tapent -->
-        <input type="password" name="class_code">
+        <div>
+            <!-- Champ pour mdp de classe, type number pour que les enfants voient ce qu'ils tapent -->
+            <input type="text" name="class_code">
+            <!-- Affichage d'erreur -->
+            @error("class_code")
+                {{ $message }}
+            @enderror
+        </div>
         <button>Valider</button>
     </form>
 @endsection
