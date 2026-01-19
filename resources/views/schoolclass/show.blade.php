856ed00b6395ef94ddec9f2e76490d96d2dd049f
Camille <camille.bachmann@eduvaud.ch>
2026-01-19 16:17:07 +0100

    feat : back button, can be used in other view

--- resources/views/schoolclass/show.blade.php
+++ resources/views/schoolclass/show.blade.php
@@ -5,9 +5,14 @@
 @section('content')
 
     <h2>Liste des élèves</h2>
+    <button onclick="window.history.back()">Retour</button>
+    <!-- Affiche tous les élèves -->
     @foreach($students as $student)
         <div>
-            <p>{{ $student->first_name }}</p>
+            <a href="#">
+                <p>{{ $student->first_name }} {{ $student->last_name }}</p>
+                <!-- A tester avec les images <img src="{{ $student->medium_id }}" alt="animal"> -->
+            </a>
         </div>
     @endforeach
 @endsection
