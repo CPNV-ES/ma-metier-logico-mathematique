94f0f1aa4ca28ad7fa7eca3541f38e1d7c51643a
T5uy0 <julienschneider964@gmail.com>
2026-01-19 10:52:08 +0100

    feat: add login and register for teacher

---
+++ resources/views/auth/teacher-login.blade.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Connexion enseignant</title>
+</head>
+<body>
+<h1>Connexion enseignant</h1>
+
+@if ($errors->any())
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
+
+<form method="POST" action="{{ route('teacher.login.store') }}">
+    @csrf
+
+    <div>
+        <label for="email">E-mail</label><br>
+        <input id="email" name="email" type="email" value="{{ old('email') }}" required autofocus>
+    </div>
+
+    <div>
+        <label for="password">Mot de passe</label><br>
+        <input id="password" name="password" type="password" required>
+    </div>
+
+    <div>
+        <label>
+            <input type="checkbox" name="remember" value="1">
+            Se souvenir de moi
+        </label>
+    </div>
+
+    <button type="submit">Se connecter</button>
+</form>
+
+<hr>
+
+<p>Pas encore de compte ?</p>
+<p><a href="{{ route('teacher.register') }}">Créer un compte</a></p>
+
+</body>
+</html>
