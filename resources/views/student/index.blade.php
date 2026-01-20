73d7049af9ce0e9fd508a4ae72c7a8964d1f852f
Camille <camille.bachmann@eduvaud.ch>
2026-01-20 09:14:27 +0100

    feat : prepare student.index for later

---
+++ resources/views/student/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layout')
+
+@section('title', 'student name') <!-- 'student name' à remplacer par qqch du genre $studentName quand on pourra return ça depuis la base -->
+
+@section('content')
+    <!-- Nom de l'élève -->
+    <h1>student name</h1>
+    <button onclick="window.history.back()">Retour</button>
+    <!-- Affiche toutes les catégories -->
+    <!-- A implémenter plus tard
+    @foreach($categories as $category)
+        <div>
+            <a href="#">
+                {{ $category->name }}
+            </a>
+        </div>
+    @endforeach
+    -->
+@endsection
