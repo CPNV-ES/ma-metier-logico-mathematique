05dd2361688fec9b61caa58535b5766f8d5130ed
T5uy0 <julienschneider964@gmail.com>
2026-01-21 14:21:44 +0100

    refactor: fix view

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -7,13 +7,14 @@
     <h1>{{$name}}</h1>
     <button onclick="window.history.back()">Retour</button>
     <!-- Affiche toutes les catégories -->
-     
+
     @foreach($categories as $category)
         <div>
             <button onclick="window.location='{{route('findcategory',['id'=>$category->id])}}'">
+                <img src="{{ $category->media->path }}" alt="">
                 {{ $category->name }}
             </button>
         </div>
     @endforeach
-        
+
 @endsection
