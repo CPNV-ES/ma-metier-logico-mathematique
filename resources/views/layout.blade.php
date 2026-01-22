3125a0d9d63b5aa279687028382c62a46b7d725f
Jean_08 <pa16wap@eduvaud.ch>
2026-01-22 13:28:28 +0100

    FE : Compte création enseignant + Connexion enseignant

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <title>@yield('title')</title>
 </head>
-<body>
-    <div class="container">
+<body class="bg-[#FFF4B8]">
+    <div>
         @yield('content')
     </div>
 </body>
